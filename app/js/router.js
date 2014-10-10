c9e13fdc010ed5b2a7e14c0ed0e9146d7c51e423
Patrick Love <plove13x@gmail.com>
2014-10-09 20:33:57 -0400

    Initial Commit

---
+++ app/js/router.js
@@ -0,0 +1,9 @@
+Crack.Router.map(function(){
+	// this.resource('todos', { path: '/' });
+});
+
+// Todos.TodosRoute = Ember.Route.extend({
+//   model: function() {
+//     return this.store.find('todo');
+//   }
+// });
